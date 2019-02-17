94f969e8b9b368044c9222ccbbf55ed8b0eb920c
TilakPatel <ptilak142@gmail.com>
2019-02-17 16:12:40 -0500

    Add files via upload

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const simpleOauthModule = require('simple-oauth2');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const info = require('./info-schema');
-mongoose.connect('mongodb+srv://tilak:Basketball23@cluster0-vyhsz.mongodb.net/test?retryWrites=true&useNewUrlParser=true').then(() => {
+mongoose.connect('mongodb+srv://tilak:Basketball23@cluster0-vyhsz.mongodb.net/db?retryWrites=true&useNewUrlParser=true').then(() => {
   console.log('Connected!');
 }).catch(() => {
   console.log('Error');
@@ -21,20 +21,20 @@ app.use(function (req, res, next) {
 
 const oauth2 = simpleOauthModule.create({
   client: {
-    id: '552582400945.554222598951',
-    secret: '24f2c9f868e6839b821f540e6b9026ee',
+    id: 'dj0yJmk9UlVqOW9lWURBSThnJnM9Y29uc3VtZXJzZWNyZXQmc3Y9MCZ4PTNl',
+    secret: '32dac19266153d081c340a7a1a6af038587e7a1f',
   },
   auth: {
-    tokenHost: 'https://slack.com',
-    tokenPath: '/api/oauth.access',
-    authorizePath: '/oauth/authorize',
+    tokenHost: 'https://api.login.yahoo.com',
+    tokenPath: '/oauth2/get_token',
+    authorizePath: '/oauth2/request_auth',
   },
 });
 
 // Authorization uri definition
 const authorizationUri = oauth2.authorizationCode.authorizeURL({
-  redirect_uri: 'https://jhacksslackbackend.herokuapp.com/callback',
-  scope: 'chat:write:bot',
+  redirect_uri: 'https://fantasynamegeneratorbackend.herokuapp.com/callback',
+  scope: '',
   state: '3(#0/!~',
 });
 
@@ -48,7 +48,7 @@ app.get('/callback', async (req, res) => {
   const code = req.query.code;
   const options = {
     code: code,
-    redirect_uri: "https://jhacksslackbackend.herokuapp.com/callback"
+    redirect_uri: "https://fantasynamegeneratorbackend.herokuapp.com/callback"
   };
   try {
     const result = await oauth2.authorizationCode.getToken(options);
@@ -56,8 +56,7 @@ app.get('/callback', async (req, res) => {
     console.log('The resulting token: ', result);
 
     const token = oauth2.accessToken.create(result);
-    console.log(token.token.access_token)
-    res.status(200).send(result)
+    res.redirect('http://fantasynamegenerator.s3-website.us-east-2.amazonaws.com/teamPicker/' + token.token.access_token);
   } catch (error) {
     console.error('Access Token Error', error.message);
     return res.status(400).json('Authentication failed');
@@ -65,9 +64,17 @@ app.get('/callback', async (req, res) => {
 });
 
 app.get('/info', (req, res) => {
-    info.findById('5c69abbe12cd952414e418fb', function (err, docs) {
+    info.findById('5c68f3476da9b31ff45eb9cb', function (err, docs) {
+        if(err){
+            console.log(err)
+        }
         res.status(200).send(docs);
+        console.log(docs)
       });
+})
+app.get('/', (req, res) => {
+
+  res.send('FORBIDDEN');
 })
 
 
